526f01acbfb2d1a54dc69a680c80fb71cbb2bbcf
steve pousty <steve.pousty@gmail.com>
2019-10-31 14:52:58 -0700

    Refactor to Flask-RESTful

--- wsgi.py
+++ wsgi.py
@@ -1,12 +1,16 @@
 import os
 import json
+from flask_restful import reqparse, Api, Resource
 
 from flask import Flask
 
 # Setup the Flask application.
 
 app = Flask(__name__)
+api = Api(app)
 
-@app.route('/')
-def index():
-    return "hello world"
+class HelloWorld(Resource):
+    def get(Resource):
+        return "Hello World"
+
+api.add_resource(HelloWorld, '/')
