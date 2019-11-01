b893a55b3476e428f8c12f317694112af60cd3c8
Steven Pousty <steve.pousty@gmail.com>
2019-11-01 12:28:22 -0700

    move back to Flask and implemented Get

--- wsgi.py
+++ wsgi.py
@@ -1,42 +1,37 @@
 import os
 import json
 import psycopg2
-from flask_restful import reqparse, Api, Resource
-from flask import Flask
+from flask import Flask, request
 
 # Setup the Flask application.
 
 app = Flask(__name__)
-api = Api(app)
 
 
-# Making sure everything works
-class HelloWorld(Resource):
-    def get(self):
-        return "Hello World"
+@app.route('/')
+def index():
+    return "Hello World"
 
 
-api.add_resource(HelloWorld, '/')
-
-
-class FireHazardStatus(Resource):
-    def get(self, parcelid):
-        print(os.getenv('db_username') + " " + os.getenv('FIREDATA_PGBOUNCER_SERVICE_HOST') + " " + os.getenv('db_password'))
-        conn = psycopg2.connect(database='firedata', user=os.getenv('db_username'),
-                                    host=os.getenv('FIREDATA_PGBOUNCER_SERVICE_HOST'),
-                                    password=os.getenv('db_password'))
-        cur = conn.cursor()
-        cur.execute("""select gid, ST_AsText(geom) as geom from assessor_parcels LIMIT 3""")
+@app.route('/parcel/firehazard/<int:parcelid>')
+def firehazard(parcelid):
+    conn = psycopg2.connect(database='firedata', user=os.getenv('db_username'),
+                            host=os.getenv('FIREDATA_PGBOUNCER_SERVICE_HOST'),
+                            password=os.getenv('db_password'))
+    cur = conn.cursor()
+    if request.method == 'GET':
+        sql_string = "select gid, firehazard from assessor_parcels where gid = " + parcelid
+        cur.execute(sql_string)
 
         rows = cur.fetchall()
-        result_string = "<h2>Here are your results: </h2>"
         for row in rows:
-            result_string += "<h3>" + str(row[0]) + ", " + str(row[1]) + "</h3>"
+            result = { "parcelid": row[0], "firehazard": str(row[1])}
 
-        cur.close()
-        conn.close()
+        return result
+    elif request.method == 'PUT':
+        return 'json'
 
-        return result_string
+    return "We Weren't Supposed To Be Called The Way You Did"
 
-
-api.add_resource(FireHazardStatus, '/parcel/firehazard/', '/parcel/firehazard/<int:parcelid>')
+    cur.close()
+    conn.close()
